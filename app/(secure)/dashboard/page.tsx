85cbfd4e3876460b6ebc896eb4bd52d42564f416
Meredith Schmidt <mschmidt@userfront.com>
2024-08-08 11:53:24 -0700

    Updating stuff

--- app/(secure)/dashboard/page.tsx
+++ app/(secure)/dashboard/page.tsx
@@ -2,8 +2,6 @@
 
 import * as React from "react";
 import { useUserfront, LogoutButton } from "@userfront/next/client";
-import UserDataComponent from './UserDataComponent';
-import Link from "next/link";
 
 export default function DashboardPage() {
   const { user } = useUserfront();
@@ -12,9 +10,7 @@ export default function DashboardPage() {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
       <p>Hello, {user.email}</p>
-      <UserDataComponent />
      <LogoutButton />
-     <Link href="/server">Server Example with Async Data Fetching &#8594;</Link>
     </div>
   );
 }
