df9e24abcce4d62d4b38a7dd9a21b79b5be30d02
Meredith Schmidt <mschmidt@userfront.com>
2024-08-07 21:30:58 -0700

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/protected-route',
+          destination: '/api/protected-route',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
